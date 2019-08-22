6971a6f0c816e96be66cb509c6bff37563723372
Henrique Guazzelli Mendes <henriqueguazzellimendes@Henriques-MBP.localdomain>
2019-08-21 22:29:18 -0300

    lab finished with GET POST implementations

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,20 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
+const hbs = require('hbs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const beers = require('./routes/beers.routes');
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
+hbs.registerPartials(`${__dirname}/views/partials`);
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-
+app.use('/', beers);
 
 app.listen(3000);
